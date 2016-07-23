c914b9e7ffc2ec27c168bfc2d71d129deb222b27
root <root@localhost.localdomain>
2016-07-23 13:38:37 +0530

    Addition is added

--- cal.sh
+++ cal.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-echo "this is first line"
+echo "Regular Calculator"
+
+ echo -e "1. Add \n 2.Sub \n 3.Mul"
+ 
+ echo "Enter choice:"
+
+ read ch
+
+ case $ch in 
+  1) echo "Enter two nos"
+     read no1
+     read no2
+     echo "Addition is:" `expr $no1 + $no2`
+     ;;
+  *) echo "wrong choice"
+ esac
+
+
+ 
